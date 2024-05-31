6fea3d8ceab943e822d80a8acfe504bfb6313549
Olivier Nicole <olivier@chnik.fr>
2024-06-01 00:38:39 +0200

    Generated iter_option!
    Remaining work:
    
    - get rid of interpretative overhead
    - support recursive types
    - work around the value restriction somehow

--- staged_iter.ml
+++ staged_iter.ml
@@ -61,6 +61,10 @@ module Staged = struct
     let spine view = view
   end
 
+  type ('q, 'res) app0 = 'res
+
+  type ('a, 'q, 'res) app1 = ('a, 'q) app code -> ('q, 'res) app0
+
   type ('a, 'x) data1 =
     (* Only ['dynq] here I think??? *)
     { view : 'statq 'dynq. ('a, 'dynq, ('x, 'statq, 'dynq) V.t) app1;
@@ -82,7 +86,7 @@ module%code Dyn = struct [@code]
     | Tpf.V.R _ -> failwith "unsupported"
 end
 
-let rec staged_g_iter_aux
+let staged_g_iter_aux
   : ('a, G_staged.p, G.p) V.t -> 'a code -> unit code =
   fun view v_code ->
     let rec go : 'a. ('a, _, G_staged.p, G.p) V.spine -> unit code =
@@ -110,19 +114,61 @@ let staged_g_iter : ('a, G_staged.p, G.p) V.t -> ('a -> unit) code =
   fun view ->
     [%code (fun v -> [%e staged_g_iter_aux view [%code v] ]) ]
 
-let staged_option_view : ('a, 'a option) Staged.data1 =
-  let rec view_aux
-    : 'statq 'dynq. ('a, 'dynq) app -> 'a option code -> ('a, 'a, 'statq, 'dynq) V.spine =
+let staged_option_data1 : ('a, 'a option) Staged.data1 =
+  let view
+    : type statq dynq. ('a, dynq) app code -> 'a option code -> ('a option, 'a option, statq, dynq) V.spine =
     fun f_a ->
       fun option_code ->
         V.{ stat = None;
             dyn =
+              (
               [%code
                 match [%e option_code ] with
                 | None -> Tpf.V.K None
                 | Some a ->
-                    Tpf.V.A (K Option.some, a, f_a)
+                    Tpf.V.A (K Option.some, a, [%e f_a ])
               ]
+              : ('a option, 'a option, dynq) Tpf.V.spine code
+              );
           }
   in
-  assert false
+  (*
+  ('a, 'a option) Staged.data1
+  =
+  { view : 'statq 'dynq. ('a, 'dynq, ('a option, 'statq, 'dynq) V.t) app1 }
+  =
+  { view : 'statq 'dynq. ('a, 'dynq, ('a option, 'statq, 'dynq) V.t) app1 }
+  =
+  { view : 'statq 'dynq. ('a, 'dynq) app -> ('a option, 'statq, 'dynq) V.t }
+  =
+  { view : 'statq 'dynq. ('a, 'dynq) app -> 'a code -> ('a option, 'a option, 'statq, 'dynq) V.spine }
+  *)
+  { view }
+
+(* How to use a [data1]? *)
+
+let iter_option_code_aux : 'a. (('a -> unit) code) -> ('a option -> unit) code =
+  fun f ->
+  staged_g_iter
+    (staged_option_data1.view
+      ([%code Dyn.G.(!) [%e f ] ] : ('a, G.p) app code)
+    )
+
+let iter_option : 'a. ('a -> unit) -> ('a option -> unit) =
+  fun f x ->
+    Ppx_stage.run
+      [%code
+        fun f x ->
+          [%e
+            staged_g_iter
+              (staged_option_data1.view
+                ([%code Dyn.G.(!) f ] : ('a, G.p) app code)
+              )
+          ]
+          x
+      ]
+      f
+      x
+
+let show () =
+  Ppx_stage.print Format.std_formatter (iter_option_code_aux [%code fun _ -> ()])
--- staged_iter.mli
+++ staged_iter.mli
@@ -4,4 +4,6 @@ type iter_p
 
 val g_iter : ('a, iter_p) Tpf.view -> 'a -> unit
 
-val list_iter : ('a -> unit) -> 'a list -> unit
+val iter_option : ('a -> unit) -> 'a option -> unit
+
+val show : unit -> unit
