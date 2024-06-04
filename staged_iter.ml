353b9f0ea3fc7432a9b9d145267006a607006327
Olivier Nicole <olivier@chnik.fr>
2024-06-04 10:28:05 +0200

    wip

--- staged_iter.ml
+++ staged_iter.ml
@@ -28,41 +28,29 @@ open Ppx_stage
 
 module Staged = struct
   module V = struct
-    type (_, _, +_, _) stat_spine =
-    | K : 'a code -> ('a, 'r, 'statq, 'dynq) stat_spine
-    | A : ('a -> 'b, 'r, 'statq, 'dynq) spine * 'a code * ('a, 'dynq) app code
-          -> ('b, 'r, 'statq, 'dynq) stat_spine
-    | ComputeResult : ('r, 'dynq) app code -> ('a, 'r, 'statq, 'dynq) stat_spine
-    (*| DynExplore : ('a -> ('a, 'dynq) app) code -> ('a, 'r, 'statq, 'dynq) stat_spine*)
-    (* Let's try without recursive types first *)
-    (*| R : ('r -> 'b, 'r, 'q) spine * 'r code -> ('b, 'r, 'q) spine*)
+    type (+_, _, +_) stat_spine =
+    | K : 'a code -> ('a, 'r, 'q) stat_spine
+    | A : ('a -> 'b, 'r, 'q) spine * 'a code * ('a, 'q) app code
+          -> ('b, 'r, 'q) stat_spine
+    (* | R : ('r -> 'b, 'r, 'q) spine * 'r code -> ('b, 'r, 'q) stat_spine *)
+    | ComputeResult : ('r, 'q) app code -> ('a, 'r, 'q) stat_spine
 
-    and ('a, 'r, +'statq, 'dynq) spine =
-      { stat : ('a, 'r, 'statq, 'dynq) stat_spine option;
-        dyn : ('a, 'r, 'dynq) V.spine code }
+    and (+'a, 'r, 'q) spine =
+      { stat : ('a, 'r, 'q) stat_spine option;
+        dyn : ('a, 'r, 'q) V.spine code }
 
-    type ('a, +'statq, 'dynq) t = 'a code -> ('a, 'a, 'statq, 'dynq) spine
+    type ('a, 'q) t = 'a code -> ('a, 'a, 'q) spine
 
-    let spine : ('a, 'statq, 'dynq) t -> 'a code -> ('a, 'a, 'statq, 'dynq) spine =
+    let spine : ('a, 'q) t -> 'a code -> ('a, 'a, 'q) spine =
       fun view -> view
       [@@warning "-unused-value-declaration"]
   end
 
-  (*
   type ('a, 'x) data1 =
-    (* Only ['dynq] here I think??? *)
-    { view : 'statq 'dynq.
-        ('a, 'dynq) app code
-        -> (('x, 'x, 'statq, 'dynq) V.spine -> ('x, 'dynq) app code)
-        -> 'x code
-        -> ('x, 'dynq) app code
-    }
-  *)
-  type ('a, 'x) data1 =
-    { explore : 'statq 'dynq.
-           ('a, 'dynq) app code
-        -> (('a option, 'statq, 'dynq) V.t -> ('a option, 'dynq) app code)
-        -> ('a option, 'dynq) app code
+    { explore : 'q.
+           ('a, 'q) app code
+        -> (('a option, 'q) V.t -> ('a option, 'q) app code)
+        -> ('a option, 'q) app code
     }
 end
 
@@ -81,13 +69,13 @@ module%code Dyn = struct [@code]
     | Tpf.V.R _ -> failwith "unsupported"
 end
 
-let staged_g_iter_aux
+let (* rec *) staged_g_iter_aux
   : type statq dynq a.
-       (a, Iterate_proxy_staged.p, Iterate_proxy.p) V.t
+       (a, Iterate_proxy.p) V.t
     -> a code
     -> unit code =
   fun view a_code ->
-    let rec go : type b. (b, _, Iterate_proxy_staged.p, Iterate_proxy.p) V.spine -> unit code =
+    let rec go : type b. (b, _, Iterate_proxy.p) V.spine -> unit code =
       fun spine ->
         match spine with
         | V.{ stat = Some (V.K _); dyn = _ } -> [%code () ]
@@ -96,30 +84,78 @@ let staged_g_iter_aux
               [%e go s ];
               Dyn.Iterate_proxy.(!:) [%e f_a ] [%e a_code ]
             ]
-        | { stat = Some (ComputeResult code); dyn = _ } ->
-            [%code Dyn.Iterate_proxy.(!:) [%e code ] [%e a_code ] ]
-        | { stat = None; dyn = spine_code } ->
-            [%code Dyn.iter_dyn_spine [%e spine_code ] ]
         (*
-        | V.R (s, a_code) ->
+        | { stat = Some (R (s, a_code)); dyn = _ } ->
             [%code
               [%e go s ];
               [%e staged_g_iter_aux view a_code ]
             ]
         *)
+        | { stat = Some (ComputeResult code); dyn = _ } ->
+            [%code Dyn.Iterate_proxy.(!:) [%e code ] [%e a_code ] ]
+        | { stat = None; dyn = spine_code } ->
+            [%code Dyn.iter_dyn_spine [%e spine_code ] ]
     in
     go (view a_code)
 
 let staged_g_iter
-  : ('a, Iterate_proxy_staged.p, Iterate_proxy.p) V.t -> ('a, Iterate_proxy.p) app code =
+  : ('a, Iterate_proxy.p) V.t -> ('a, Iterate_proxy.p) app code =
   fun view ->
     [%code Dyn.Iterate_proxy.(!) (fun v -> [%e staged_g_iter_aux view [%code v] ]) ]
 
+(*
+let list_data1 : ('a, 'a list) Staged.data1 =
+  let explore
+    : type q.
+      ('a, q) app code
+      -> (('a list, q) V.t -> ('a list, q) app code)
+      -> ('a list, q) app code =
+    fun f_a view_consumer ->
+      view_consumer (fun (a_code : 'a list code) ->
+        { stat = Some (V.ComputeResult
+            [%code
+            match [%e a_code ] with
+            | [] ->
+                [%e
+                  view_consumer
+                    (fun _ ->
+                      V.{ stat = Some (K [%code [] ]);
+                          dyn = [%code Tpf.V.K [] ]
+                        }
+                    )
+                ]
+            | x :: xs ->
+                [%e
+                  let remaining_spine =
+                    V.{ stat = Some (K [%code List.cons ]);
+                        dyn = [%code Tpf.V.K List.cons ]
+                      }
+                  in
+                  view_consumer
+                    (fun _ ->
+                      V.{ stat = Some (R (remaining_spine, [%code xs ]));
+                          dyn = [%code
+                            Tpf.V.R ([%e remaining_spine.dyn ], xs) ]
+                        }
+                    )
+                ]
+            ]);
+          dyn =
+            [%code
+              match [%e a_code ] with
+              | [] -> Tpf.V.K []
+              | x :: xs -> Tpf.V.R (K x, xs)
+            ]
+        })
+  in
+  { explore }
+*)
+
 let staged_option_data1 : ('a, 'a option) Staged.data1 =
   let explore
-    : type statq dynq.
+    : type dynq.
          ('a, dynq) app code
-      -> (('a option, statq, dynq) V.t -> ('a option, dynq) app code)
+      -> (('a option, dynq) V.t -> ('a option, dynq) app code)
       -> ('a option, dynq) app code =
       (*-> ('a option, 'a option, statq, dynq) V.spine =*)
     fun f_a view_consumer ->
@@ -166,12 +202,12 @@ let staged_option_data1 : ('a, 'a option) Staged.data1 =
   (*
   ('a, 'a option) Staged.data1
   =
-  { view : 'statq 'dynq. ('a, 'dynq, ('a option, 'statq, 'dynq) V.t) app1 }
+  { view : 'dynq. ('a, 'dynq, ('a option, 'dynq) V.t) app1 }
   =
-  { view : 'statq 'dynq. ('a, 'dynq) app code -> ('a option, 'statq, 'dynq) V.t }
+  { view : 'dynq. ('a, 'dynq) app code -> ('a option, 'dynq) V.t }
   =
-  { view : 'statq 'dynq. ('a, 'dynq) app code
-                         -> (('a option, 'a option, 'statq, 'dynq) V.spine -> ('a option, 'dynq) app code)
+  { view : 'dynq. ('a, 'dynq) app code
+                         -> (('a option, 'a option, 'dynq) V.spine -> ('a option, 'dynq) app code)
                          -> 'a option code
                          -> ('a option, 'dynq) app code }
   *)
@@ -179,19 +215,21 @@ let staged_option_data1 : ('a, 'a option) Staged.data1 =
 
 (* How to use a [data1]? *)
 
-let iter_option_code_aux : ('a -> unit) code -> ('a option, Iterate_proxy.p) app code =
+let iter_option_code_aux : 'a. ('a -> unit) code -> ('a option, Iterate_proxy.p) app code =
   fun f ->
   staged_option_data1.explore
     ([%code Dyn.Iterate_proxy.(!) [%e f ] ] : ('a, Iterate_proxy.p) app code)
     (staged_g_iter)
 
-let iter_option : 'a. ('a -> unit) -> 'a option -> unit =
+let iter_option : ('a -> unit) -> 'a option -> unit =
   fun f opt ->
   Ppx_stage.run (
     [%code
       fun f opt -> Dyn.Iterate_proxy.(!:) [%e iter_option_code_aux [%code f ] ] opt
     ]
-  ) f opt
+  )
+  f
+  opt
 
 let show () =
   Ppx_stage.print
