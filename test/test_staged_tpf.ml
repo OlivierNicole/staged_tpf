bd5e767322a5ccb820f5c233d28e63ac6d730ab3
Olivier Nicole <olivier@chnik.fr>
2024-06-08 01:35:11 +0200

    Restructure

---
+++ test/test_staged_tpf.ml
@@ -0,0 +1,50 @@
+module M = Staged_tpf.Staged_iter
+
+let () =
+  Format.printf "@[<v>";
+  Format.printf "------@,";
+  M.show ();
+  Format.printf "@,";
+  Format.printf "------@,";
+  M.iter_list (Format.printf "%d@,") [42; (-1); (-898)];
+  (*M.iter_option_list (Format.printf "%d@,") [Some 42; Some (-1); None; Some (-898)];*)
+  Format.printf "------@,";
+
+[@@@warning "-ignored-partial-application"]
+[@@@warning "-unused-value-declaration"]
+[@@@warning "-unused-var-strict"]
+[@@@warning "-unused-rec-flag"]
+
+module Dyn'1 = struct
+  module Iterate_proxy = struct
+    external (!) : 'a -> 'a = "%identity"
+    external (!:) : 'a -> 'a = "%identity"
+  end
+end
+
+let _ =
+  fun f ->
+    fun l ->
+        (
+           (fun v ->
+              let rec fix x =
+                match x with
+                | [] -> ()
+                | x::xs ->
+                    ((();
+                        (
+                           (fun v ->
+                                (
+                                   (fun v ->
+                                      let rec fix x =
+                                        match x with
+                                        | None -> ()
+                                        | Some a ->
+                                            (();
+                                               (
+                                                  (fun v ->
+                                                     (Format.printf "%d\n") v))
+                                               a) in
+                                      fix v)) v)) x);
+                     fix xs) in
+              fix v)) l
